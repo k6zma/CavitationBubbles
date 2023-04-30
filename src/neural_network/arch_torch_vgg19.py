3ff119ead5e42dfe07c40e9c65ffe68c27fcdf4e
k6zma <k6zma@yandex.ru>
2023-04-30 12:00:48 +0300

    added script for vgg16 model

--- src/neural_network/arch_torch_vgg19.py
+++
@@ -1,7 +0,0 @@
-from torchview import draw_graph 
-import torch
-model = torch.load('data/model/model_torch/model_torch_vgg19.pth')
-batch_size = 32
-# device='meta' -> no memory is consumed for visualization 
-model_graph = draw_graph(model, input_size=(batch_size, 64), device='meta') 
-model_graph.visual_graph
